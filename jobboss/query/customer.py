9cfcc9f37b3fd8c6a1db88b4f363ef3f5cd14936
Scott Sawyer <ssawyer06@gmail.com>
2019-04-23 15:02:59 -0400

    Contact and Address get_or_create logic with tests

--- jobboss/query/customer.py
+++ jobboss/query/customer.py
@@ -1,55 +1,20 @@
 import datetime
 import re
 from django.template.defaultfilters import slugify
-from jobboss.models import Customer
+from jobboss.models import Customer, Contact, Address
 
 MAX_CODE_LENGTH = 10  # max length of Customer PK
 MAX_CODE_RETRIES = 20
+MAX_ADDR_CODE_LENGTH = 6  # max length of Ship_To_ID
 
+"""
+Queries for finding and creating Customer, Contact, and Address records using
+fuzzy record matching to help avoid duplicate records.
 
-def tokenize(name):
-    return slugify(name).split('-')
-
-
-def filter_exact_customer_name(name):
-    return Customer.objects.filter(name__iexact=name).order_by(
-        '-last_updated').first()
-
-
-def filter_fuzzy_customer_name(name):
-    qs = Customer.objects
-    for token in tokenize(name):
-        qs = qs.filter(name__icontains=token)
-    qs.order_by('-last_updated')
-    return qs.first()
-
-
-def increment_code(code):
-    search = re.search('(\d+)$', code)
-    if search is None:
-        if len(code) < MAX_CODE_LENGTH:
-            return code + '1'
-        else:
-            return code[0:MAX_CODE_LENGTH-1] + '1'
-    else:
-        suffix = search.group(0)
-        base_code = code[0:-len(suffix)]
-        new_suffix = str(int(search.group(0)) + 1)
-        remove = len(base_code) + len(new_suffix) - MAX_CODE_LENGTH
-        if remove > 0:
-            base_code = base_code[0:-remove]
-        return base_code + new_suffix
-
-
-def get_available_customer_code(name):
-    code = name.upper()[0:MAX_CODE_LENGTH]
-    tries = 0
-    while tries < MAX_CODE_RETRIES:
-        if Customer.objects.filter(customer=code).count() == 0:
-            return code
-        code = increment_code(code)
-        tries += 1
-    raise ValueError("Can't find an unused customer code for {}".format(name))
+* get_or_create_customer
+* get_or_create_contact
+* get_or_create_address
+"""
 
 
 def get_or_create_customer(name: str) -> Customer:
@@ -75,3 +40,180 @@ def get_or_create_customer(name: str) -> Customer:
         send_report_by_email=False
     )
     return customer
+
+
+def get_or_create_contact(customer: Customer, contact_name: str) -> Contact:
+    """
+    Find existing Contact record for this Customer or create a new Contact.
+
+    :param customer: Customer record
+    :param contact_name: Contact.Contact_Name to be fuzzy-matched
+    :return: matched or newly created JobBOSS Contact record
+    """
+    contact = filter_exact_contact_name(customer, contact_name)
+    if contact is not None:
+        return contact
+    contact = Contact.objects.create(
+        customer=customer.customer,
+        contact_name=contact_name,
+        last_updated=datetime.datetime.utcnow()
+    )
+    return contact
+
+
+def get_or_create_address(
+        customer: Customer, addr_dict: dict, is_shipping: bool=True
+) -> Address:
+    """
+    Find existing Address record for this Customer or a create a new Address.
+
+    :param customer: Customer record
+    :param addr_dict: Dictionary containing address fields. Generate using
+    ``paperless.objects.Address.to_dict()``.
+    :param is_shipping: Set to True if shipping address; otherwise False
+    :return: matched or newly created JobBOSS Address record
+    """
+    address = match_address(customer, addr_dict)
+    if address is not None:
+        if is_shipping and not address.shippable:
+            address.shippable = True
+            address.save()
+        if not is_shipping and not address.billable:
+            address.billable = True
+            address.save()
+        return address
+    if addr_dict.get('country') == 'USA':
+        country_code = 'US'
+    else:
+        country_code = addr_dict.get('country')
+    phone = addr_dict.get('phone')
+    if phone and addr_dict.get('phone_ext'):
+        phone += ' x{}'.format(addr_dict.get('phone_ext'))
+    address = Address.objects.create(
+        customer=customer,
+        status='Active',
+        type='000',
+        ship_to_id=get_available_address_code(customer, ship=is_shipping),
+        line1=addr_dict.get('line1'),
+        line2=addr_dict.get('line2'),
+        city=addr_dict.get('city'),
+        state=addr_dict.get('state'),
+        zip=addr_dict.get('postal_code'),
+        country=country_code,
+        name='{} {}'.format(addr_dict.get('first_name'),
+                            addr_dict.get('last_name')).strip(),
+        phone=phone,
+        lead_days=0,
+        last_updated=datetime.datetime.utcnow(),
+        billable=not is_shipping,
+        shippable=is_shipping
+    )
+    return address
+
+
+"""
+Helper methods for get_or_create queries
+"""
+
+
+def tokenize(name):
+    return slugify(name).split('-')
+
+
+def filter_exact_customer_name(name):
+    return Customer.objects.filter(name__iexact=name).order_by(
+        '-last_updated').first()
+
+
+def filter_exact_contact_name(customer: Customer, contact_name):
+    return Contact.objects.filter(
+        customer=customer.customer,
+        contact_name__iexact=contact_name).order_by('-last_updated').first()
+
+
+def filter_fuzzy_customer_name(name):
+    qs = Customer.objects
+    for token in tokenize(name):
+        qs = qs.filter(name__icontains=token)
+    qs.order_by('-last_updated')
+    for customer in qs.all():
+        if slugify(customer.name) == slugify(name):
+            return customer
+    return None
+
+
+def increment_code(code, max_code_length=MAX_CODE_LENGTH):
+    search = re.search('(\d+)$', code)
+    if search is None:
+        if len(code) < max_code_length:
+            return code + '1'
+        else:
+            return code[0:max_code_length-1] + '1'
+    else:
+        suffix = search.group(0)
+        base_code = code[0:-len(suffix)]
+        new_suffix = str(int(search.group(0)) + 1)
+        remove = len(base_code) + len(new_suffix) - max_code_length
+        if remove > 0:
+            base_code = base_code[0:-remove]
+        return base_code + new_suffix
+
+
+def get_available_customer_code(name):
+    code = name.upper()[0:MAX_CODE_LENGTH]
+    tries = 0
+    while tries < MAX_CODE_RETRIES:
+        if Customer.objects.filter(customer=code).count() == 0:
+            return code
+        code = increment_code(code)
+        tries += 1
+    raise ValueError("Can't find an unused customer code for {}".format(name))
+
+
+def match_address(customer: Customer, addr_dict: dict) -> Address:
+    """
+    Find a matching address (case insensitive) for a given customer looking at
+    concatenated name, address line 1 and 2, city, state, zip, country, and
+    concatenated phone number.
+
+    :param customer: Customer record
+    :param addr_dict: Dictionary containing address fields. Generate using
+    ``paperless.objects.Address.to_dict()``.
+    :return: Address if found; otherwise None
+    """
+    if addr_dict.get('country') == 'USA':
+        country_code = 'US'
+    else:
+        country_code = addr_dict.get('country')
+    phone = addr_dict.get('phone')
+    if phone and addr_dict.get('phone_ext'):
+        phone += ' x{}'.format(addr_dict.get('phone_ext'))
+    qs = Address.objects.filter(
+        customer=customer,
+        name__iexact='{} {}'.format(addr_dict.get('first_name', ''),
+                                    addr_dict.get('last_name', '')).strip(),
+        line1__iexact=addr_dict.get('line1'),
+        line2__iexact=addr_dict.get('line2'),
+        city__iexact=addr_dict.get('city'),
+        state__iexact=addr_dict.get('state'),
+        zip=addr_dict.get('postal_code'),
+        country=country_code,
+        phone=phone
+    ).order_by('-last_updated')
+    return qs.first()
+
+
+def get_available_address_code(customer: Customer, ship=True):
+    if ship:
+        code = 'SHIP'
+    else:
+        code = 'BILL'
+    tries = 0
+    while tries < MAX_CODE_RETRIES:
+        if Address.objects.filter(customer=customer,
+                                  ship_to_id=code).count() == 0:
+            return code
+        code = increment_code(code, MAX_ADDR_CODE_LENGTH)
+        tries += 1
+    raise ValueError("Can't find an unused address code for {}".format(
+        customer.customer))
--- jobboss/tests/test_customer.py
+++ jobboss/tests/test_customer.py
@@ -2,23 +2,63 @@ import datetime
 from django.test import TransactionTestCase
 from jobboss.query.customer import tokenize, filter_exact_customer_name, \
     filter_fuzzy_customer_name, increment_code, get_available_customer_code, \
-    get_or_create_customer
-from jobboss.models import Customer
+    get_or_create_customer, get_or_create_contact, match_address, \
+    get_available_address_code, get_or_create_address
+from jobboss.models import Customer, Contact, Address
 
 CUST_CODE = 'CUST01'
 JB_NAME = 'CUSTOMER 1'
 PP_NAME = 'Customer 1 '
+JB_CONTACT_NAME = 'JOHN SMITH'
+PP_CONTACT_NAME = 'John Smith'
+ADDR_DICT = {
+    'business_name': 'Customer 1',
+    'city': 'Washington',
+    'country': 'USA',
+    'first_name': 'John',
+    'last_name': 'Smith',
+    'line1': '1600 Penn Ave',
+    'line2': None,
+    'phone': None,
+    'phone_ext': None,
+    'postal_code': '20500',
+    'state': 'DC'
+}
 
 
 class TestCustomer(TransactionTestCase):
     def setUp(self):
-        Customer.objects.create(
+        customer = Customer.objects.create(
             customer=CUST_CODE,
             name=JB_NAME,
             last_updated=datetime.datetime.utcnow(),
             print_statement=False,
             accept_bo=False,
             send_report_by_email=False
+        )
+        Contact.objects.create(
+            customer=CUST_CODE,
+            contact_name=JB_CONTACT_NAME,
+            last_updated=datetime.datetime.utcnow()
+        )
+        Address.objects.create(
+            customer=customer,
+            status='Active',
+            type='000',  # not sure
+            ship_to_id='SHIP',
+            line1=ADDR_DICT['line1'].upper(),
+            line2=ADDR_DICT['line2'],
+            city=ADDR_DICT['city'].upper(),
+            state=ADDR_DICT['state'].upper(),
+            zip=ADDR_DICT['postal_code'],
+            name='{} {}'.format(ADDR_DICT['first_name'],
+                                ADDR_DICT['last_name']).upper(),
+            country='US',
+            phone=ADDR_DICT['phone'],
+            lead_days=0,
+            last_updated=datetime.datetime.utcnow(),
+            billable=False,
+            shippable=True
         )
 
     def test_tokenize(self):
@@ -32,6 +72,7 @@ class TestCustomer(TransactionTestCase):
     def test_fuzzy_name_match(self):
         self.assertIsNone(filter_fuzzy_customer_name('bad name'))
         self.assertEqual(JB_NAME, filter_fuzzy_customer_name(PP_NAME).name)
+        self.assertIsNone(filter_fuzzy_customer_name('1 customer'))
 
     def test_code_mutation(self):
         self.assertEquals('CUST1', increment_code('CUST'))
@@ -39,6 +80,8 @@ class TestCustomer(TransactionTestCase):
         self.assertEquals('CUSTOMERA1', increment_code('CUSTOMERAB'))
         self.assertEquals('CUSTOMER10', increment_code('CUSTOMERA9'))
         self.assertEquals('Strange c1', increment_code('Strange case 13 '))
+        self.assertEquals('ABCDE1', increment_code('ABCDEF', 6))
+        self.assertEquals('ABCDE2', increment_code('ABCDE1', 6))
 
     def test_available_code(self):
         self.assertEqual('TEST', get_available_customer_code('test'))
@@ -53,3 +96,46 @@ class TestCustomer(TransactionTestCase):
         self.assertEqual(c+1, Customer.objects.count())
         self.assertEqual('PAPERLESS', customer.customer)
         self.assertEqual('Paperless', customer.name)
+
+    def test_contact_get_or_create(self):
+        customer = Customer.objects.first()
+        c = Contact.objects.count()
+        contact = get_or_create_contact(customer, PP_CONTACT_NAME)
+        self.assertEqual(c, Contact.objects.count())
+        self.assertEqual(contact.contact_name, JB_CONTACT_NAME)
+        contact = get_or_create_contact(customer, 'Jane Smith')
+        self.assertEqual(c+1, Contact.objects.count())
+        self.assertEqual(contact.contact_name, 'Jane Smith')
+        self.assertEqual(CUST_CODE, contact.customer)
+
+    def test_match_address(self):
+        customer = Customer.objects.first()
+        address = match_address(customer, ADDR_DICT)
+        self.assertEqual(ADDR_DICT['line1'].upper(), address.line1)
+        d = ADDR_DICT.copy()
+        d['line1'] = '1600 Pennsylvania Ave NW'  # not a match
+        self.assertIsNone(match_address(customer, d))
+
+    def test_address_code(self):
+        customer = Customer.objects.first()
+        self.assertEqual('BILL',
+                         get_available_address_code(customer, ship=False))
+        self.assertEqual('SHIP1',
+                         get_available_address_code(customer, ship=True))
+
+    def test_address_get_or_create(self):
+        customer = Customer.objects.first()
+        c = Address.objects.count()
+        address = get_or_create_address(customer, ADDR_DICT, True)
+        self.assertEqual(c, Address.objects.count())
+        self.assertEqual(ADDR_DICT['line1'].upper(), address.line1)
+        self.assertFalse(address.billable)
+        address = get_or_create_address(customer, ADDR_DICT, False)
+        self.assertTrue(address.billable)
+        d = ADDR_DICT.copy()
+        d['line1'] = '1600 Pennsylvania Ave NW'  # not a match
+        address = get_or_create_address(customer, d, True)
+        self.assertEqual(c+1, Address.objects.count())
+        self.assertTrue(address.shippable)
+        self.assertFalse(address.billable)
+        self.assertTrue(address.ship_to_id.startswith('SHIP'))
