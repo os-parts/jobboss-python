edb75e1e111f267ae100e249c929fc61dae5f5d1
Reid Lance <reidlance@Reids-MacBook-Pro-2.local>
2020-11-12 17:19:06 -0500

    adding template matching function

--- jobboss/query/job.py
+++ jobboss/query/job.py
@@ -188,3 +188,15 @@ def get_available_job(order_number: int, sequence_number: int) -> str:
 def get_material(part_number: str) -> Material:
     pn = part_number.strip()
     return Material.objects.filter(material__iexact=pn).first()
+
+
+def get_template_job(part_number: str) -> Job:
+    job = None
+    pn = part_number
+    if pn:
+        pn = part_number.strip()
+        if Job.objects.filter(part_number=pn).count() > 0:
+            job = Job.objects.filter(part_number=pn).last()
+
+
+    return job
