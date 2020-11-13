703e8ed4180a767caca055060e3647fc8e7ebcb6
Reid Lance <reid.lance@paperlessparts.com>
2020-11-12 22:21:57 -0500

    added logic to handle null address

--- jobboss/query/customer.py
+++ jobboss/query/customer.py
@@ -117,6 +117,23 @@ def get_or_create_address(
         if needs_save:
             address.save()
         return address
+    else:
+        address_array = []
+        values = Address.objects.filter(customer=customer).order_by('-last_updated')
+        if len(values) > 1:
+            values = list(values)
+            for value in values:
+                address_array.append(value)
+        elif len(values) == 1:
+            address_array.append(values)
+        for address in address_array:
+            is_def_bill = int(address.type[1])
+            is_def_ship = int(address.type[2])
+            if is_shipping and is_def_ship == 1:
+                return address
+            elif not is_shipping and is_def_bill == 1:
+                return address
+
     if addr_dict.get('country') == 'USA':
         country_code = 'US'
     else:
