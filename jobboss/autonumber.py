b2ce104c90882080b75fc682ac41045becbd26fe
Monika Wilson <monika.wilson@paperlessparts.com>
2021-05-23 15:06:03 -0400

    Add prefix support to save_with_autonumber

--- jobboss/autonumber.py
+++ jobboss/autonumber.py
@@ -18,7 +18,7 @@ class AutoNumberMixin(models.Model):
     class Meta:
         abstract = True
 
-    def save_with_autonumber(self, *args, **kwargs):
+    def save_with_autonumber(self, prefix=None, *args, **kwargs):
         assert hasattr(self, 'auto_number_attrs'), \
             'Model class must have `auto_number_attrs` attribute to use ' \
             'AutoNumberMixin'
@@ -26,15 +26,18 @@ class AutoNumberMixin(models.Model):
             auto_type: AutoColumnType
             for auto_type in self.auto_number_attrs:
                 if not getattr(self, auto_type.attr_name):
-                    auto_type.set_column(self)
-            self.save(*args, **kwargs)
+                    auto_type.set_column(self, prefix=prefix)
+            new_kwargs = kwargs
+            if 'prefix' in new_kwargs:
+                del new_kwargs['prefix']
+            self.save(*args, **new_kwargs)
 
 
 class AutoColumnType:
     def __init__(self, attr_name):
         self.attr_name = attr_name
 
-    def set_column(self, record):
+    def set_column(self, record, prefix=None):
         raise NotImplemented
 
 
@@ -43,17 +46,20 @@ class AutoNumberColumn(AutoColumnType):
         super().__init__(attr_name)
         self.jb_name = jb_name
 
-    def set_column(self, record):
+    def set_column(self, record, prefix=None):
         from jobboss.models import AutoNumber
         qs = AutoNumber.objects.select_for_update().filter(type=self.jb_name)
         an_row: AutoNumber = qs.first()
-        setattr(record, self.attr_name, str(int(an_row.last_nbr) + 1))
+        if prefix is not None:
+            setattr(record, self.attr_name, prefix + str(int(an_row.last_nbr) + 1))
+        else:
+            setattr(record, self.attr_name, str(int(an_row.last_nbr) + 1))
         an_row.last_nbr = str(int(an_row.last_nbr) + 1)
         an_row.save()
 
 
 class AutoIncrementColumn(AutoColumnType):
-    def set_column(self, record):
+    def set_column(self, record, prefix=None):
         manager = record.__class__.objects
         max_value = manager.select_for_update().all().aggregate(
             Max(self.attr_name))['{}__max'.format(self.attr_name)] or 0
